b321b0e433556aa80c69fd4a65832a937fd2033f
Claudio Junior <Cjrnew@gmail.com>
2021-03-18 14:26:57 -0300

    Projeto Criado

---
+++ src/test/java/com/cjrnew/BrewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjrnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
