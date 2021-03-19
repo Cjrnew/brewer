3da4bf58eef4022c657adec2139e9b29bc551fb0
Claudio Junior <Cjrnew@gmail.com>
2021-03-18 21:03:26 -0300

    Alterações no pom, webconfig e mailconfig

---
+++ src/main/java/com/cjrnew/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.cjrnew.config;
+
+import javax.cache.Caching;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.jcache.JCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableCaching
+@EnableAsync
+public class WebConfig implements WebMvcConfigurer {
+	
+	//BrewerDialect -> anotar @component
+	//EstiloConverter -> anotar @component
+	//CidadeCon.. -> anotar @component
+	//Estadocon... -> anotar @component
+	//GrupoCon... -> anotar @component
+	
+	@Bean
+	public CacheManager cacheManager() throws Exception {		
+		return new JCacheCacheManager(Caching.getCachingProvider().getCacheManager(
+				getClass().getResource("/env/ehcache.xml").toURI(),
+				getClass().getClassLoader()));
+	}
+	
+}
